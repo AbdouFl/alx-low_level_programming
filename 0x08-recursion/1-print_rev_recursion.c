1613b4c6ca7d1a4cb2f761861502bc739a5a58a3
AbdouFl <abderrahimfilaliofficial@gmail.com>
2023-10-01 15:27:44 -0700

    1-print_rev_recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,26 +1,25 @@
 #include "main.h"
 
 /*
- *  * Task 1. Why is it so important to dream? Because,
- *   * in my dreams we are together
- *
-  */
+ * Task 1. Why is it so important to dream? Because,
+ * in my dreams we are together
+ */
 
 /**
- *   * _print_rev_recursion - Prints a string in reverse
- *     *
- *       * @s: Input string pointer (char)
- *         *
- *           * Return: None
+  * _print_rev_recursion - Prints a string in reverse
+  *
+  * @s: Input string pointer (char)
+  *
+  * Return: None
   */
 
 void _print_rev_recursion(char *s)
 {
-		/* Check the end of the string */
-		if (*s != '\0')
-		{
+	/* Check the end of the string */
+	if (*s != '\0')
+	{
 		/* Recall the function */
-				_print_rev_recursion(s + 1);
-				_putchar(*s);
-		}
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
