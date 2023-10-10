5640ff48c3923cc1c39b8b2b20f4c94264cface5
AbdouFl <abderrahimfilaliofficial@gmail.com>
2023-10-10 14:21:48 -0700

    2 main

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 /*
- * Task of file name
+ * main - Prints the name of the file it was compiled from,
+ * followed by a new line
  *
  * Return: always 0.
- *
  */
+
 int main(void)
+
 {
+	/* Getting the filename from predefined Macros */
 	printf("%s\n", __FILE__);
 	return (0);
 }
